74f9070206bc1c020c6ff7d2c8a5ef8d250c5951
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2022-12-13 19:22:46 -0500

    Complete first draft for class.

--- Loop_Exercise.py
+++ Loop_Exercise.py
@@ -4,9 +4,14 @@
 
 #------------------------------------------------------------------------------------
 # Task: Write a program which will calculate the factorial of any integer input:
+# Remember:
 # - For negative numbers, remember that the factorial does not exist.
 # - The factorial of zero is 1.
 # - The factorial of a positive number, such as 5, is = 5*4*3*2*1 = 120.
+# The code should include:
+# - IF statements
+# - An input prompt
+# - A FOR Loop
 #------------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------------
@@ -28,10 +33,13 @@ elif num == 0:
     print("The factorial of 0 is 1")
 # Calculate the factorial of any positive integer:
 else:
-    for i in range(1, int(num) + 1): # 1 is added to the upper limit of the range since
-        # multiply factorial by current number
+    for i in range(1, int(num)+1):
+# 1 is added to the upper limit of the range since the indexes of the range are not the actual values being calculated.
+# multiply factorial by current number
         factorial = factorial * i
+# This line calculates factorials based on each successive multiple of "i."
     print("The factorial of", num, "is", factorial)
+# Print the solution to the factorial.
 
 #------------------------------------------------------------------------------------
 # End of code
